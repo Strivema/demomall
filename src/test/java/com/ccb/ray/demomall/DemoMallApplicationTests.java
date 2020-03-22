c2fd142994ab353303739ff1eee3992d1c4290cf
marui <markmarui@vip.qq.com>
2020-03-22 14:08:23 +0800

    Initial commit

---
+++ src/test/java/com/ccb/ray/demomall/DemoMallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ccb.ray.demomall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
